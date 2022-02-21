7840cfca30297e98b42cabba1ac6acc609e76c3b
Torjmen-wadie <58140901+Torjmen-wadie@users.noreply.github.com>
2022-02-21 12:28:24 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,70 @@
+
+import * as THREE from 'three'
+
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const loader = new GLTFLoader()
+loader.load('/models/animationv3.glb',function(glb)
+{
+    console.log(glb)
+    const root = glb.scene;
+
+    root.scale.set(0.2,0.2,0.2)
+    scene.add(root);
+},function (xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log ('An error occured')
+}
+)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(50,50,50)
+scene.add(light)
+//const geometry = new THREE.BoxGeometry(1,1,1)
+//const material = new THREE.MeshBasicMaterial({
+  //  color: 0x00ff00
+//})
+
+//const boxMesh = new THREE.Mesh(geometry,material)
+//scene.add(boxMesh)
+
+ 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera (75, sizes.width/sizes.height, 0.1, 100)
+
+camera.position.set(0,1,2)
+scene.add(camera)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+})
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled= true
+renderer.gammOuput = true
+
+
+
+
+renderer.render(scene,camera)
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+} 
+
+animate()
